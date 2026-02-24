5ee7ad17d3faf5c63302f943d82198ca83875c44
Adios <2722783643@qq.com>
2026-02-24 20:57:48 +0800

    feat: init project

---
+++ src/main/java/com/petlove/weblove/PetLoveWebApplication.java
@@ -0,0 +1,13 @@
+package com.petlove.weblove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLoveWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLoveWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlove/weblove/PetLoveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlove.weblove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLoveWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
